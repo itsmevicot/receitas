c4e82d526ccc35b07298f0bfa57d482e030ca4e9
Victor <victorchagasms@gmail.com>
2022-01-30 00:11:04 -0300

    aprendendo algumas features do django

--- receitas/models.py
+++ receitas/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+
+class Receita(models.Model):
+    nome_receita = models.CharField(max_length=200)
--- receitas/views.py
+++ receitas/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('<h1>Receitas</h1> <h2>Bem vindo</h2> <h3>blablabla</h3>')
+    receitas = {
+        1: 'Lasanha',
+        2: 'Sopa de Legumes',
+        3: 'Sorvete',
+        4: 'Bolo de chocolate'
+    }
+    dados = {
+        'nome_das_receitas' : receitas
+    }
+    return render(request,'index.html', dados)
+
+def receita(request):
+    return render(request, 'receita.html')
